43cb126747e88438b845cc33a6660bd920372056
Segun Samuel <68255937+Shegun-istaken@users.noreply.github.com>
2023-04-21 10:01:27 +0100

    added firebase hosting

---
+++ src/hosting.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC6Ha7FHenC25XVV_F3fBkir6vOId6jVFI",
+  authDomain: "ui-cgpa-e31be.firebaseapp.com",
+  projectId: "ui-cgpa-e31be",
+  storageBucket: "ui-cgpa-e31be.appspot.com",
+  messagingSenderId: "273341650704",
+  appId: "1:273341650704:web:52d63b23755f128b98b795"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
